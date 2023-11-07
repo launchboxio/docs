3935cf76f412001f7e2c9b9bf5d1c066fb5cefdd
Robert Wittman <robkwittman@gmail.com>
2023-11-06 22:54:04 -0500

    Ignore broken links for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'launchboxio', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
